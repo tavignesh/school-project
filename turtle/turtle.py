2f5a25935508f958282069e7fd17614f6cfad7e5
teamteroid <91048514+teamteroid@users.noreply.github.com>
2021-09-20 12:30:18 +0530

    Add files via upload

--- turtle/turtle.py
+++ turtle/turtle.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import time
+import tkinter.messagebox
+
 
 def game(player1, mainwindow):
     window = Toplevel(mainwindow)
     window.geometry("600x300")
-    p1 = PhotoImage(file = './turtle/assets/turtle1.png')
+    p1 = PhotoImage(file='./turtle/assets/turtle1.png')
     window.iconphoto(False, p1)
     window.title("Turtle Game | VVLC")
     global winner, loser, player1pos, player2pos
@@ -15,13 +17,8 @@ def game(player1, mainwindow):
     loser = None
 
     def gameover(winner, loser):
-        winimg = Image.open("./turtle/assets/winner.png")
-        #winimg = img2.resize((200,50), Image.ANTIALIAS)
-        winimg = ImageTk.PhotoImage(winimg)
-        canvas.create_image(300, 150, image=winimg)
-
-    def save():
-        savebutton["state"] = "disabled"
+        tkinter.messagebox.showinfo("Game Over", f"{winner} defeated {loser}")
+        # TODO: save score here
 
     def exit():
         window.destroy()
@@ -39,29 +36,23 @@ def game(player1, mainwindow):
                 print("Player 1 Wins")
                 winner = 1
                 loser = 2
-                savebutton.pack()
                 exitbutton.pack()
                 gameover(winner, loser)
             elif player2pos == 100:
                 print("Player 2 Wins")
                 winner = 2
                 loser = 1
-                savebutton.pack()
                 exitbutton.pack()
                 gameover(winner, loser)
 
-    #global button2
-    savebutton =  Button(window, text="Save", command=save)
-    #global button1
-    exitbutton =  Button(window, text="EXIT", command=exit)
-
-    canvas = Canvas(window, width = 500, height = 250)
+    exitbutton = Button(window, text="EXIT", command=exit)
+    canvas = Canvas(window, width=500, height=250)
     canvas.pack()
     img1 = Image.open("./turtle/assets/turtle2.png")
-    img1 = img1.resize((50,50), Image.ANTIALIAS)
+    img1 = img1.resize((50, 50), Image.ANTIALIAS)
     img1 = ImageTk.PhotoImage(img1)
     img2 = Image.open("./turtle/assets/turtle3.png")
-    img2 = img2.resize((50,50), Image.ANTIALIAS)
+    img2 = img2.resize((50, 50), Image.ANTIALIAS)
     img2 = ImageTk.PhotoImage(img2)
     player1 = canvas.create_image(0, 80, image=img1)
     player2 = canvas.create_image(0, 150, image=img2)
